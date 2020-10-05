404ede15ad011e1c3181a5cac2d13b8cc5b4d499
sashakang <44499389+sashakang@users.noreply.github.com>
2020-10-05 22:32:08 +0300

    augmentation polish

--- augmentation.py
+++ augmentation.py
@@ -12,23 +12,18 @@ transform = A.Compose([
     A.ElasticTransform(p=0.5)
 ])
 
-folder = '../pepe pix'
+infolder = '../pepe pix'
+outfolder = '../pepe transformed'
 
-for (dirpath, dirnames, filenames) in os.walk(folder):
+for (dirpath, dirnames, filenames) in os.walk(infolder):
     filenames = [name for name in filenames if name.split('.')[1] == 'png']
-    print(filenames)
-    print(len(filenames))
 
 for filename in filenames:
-    image = cv2.imread(f'{folder}/{filename}')
+    image = cv2.imread(f'{infolder}/{filename}')
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     for i in range(3):
         transformed = transform(image=image)
         transformed_image = transformed["image"]
-        cv2.imwrite(f'../pepe transformed/{filename.split(".")[0]}_{i}.png',
+        cv2.imwrite(f'../{outfolder}/{filename.split(".")[0]}_{i}.png',
                     cv2.cvtColor(transformed_image, cv2.COLOR_RGB2BGR))
-
-    # cv2.imshow("Original", image)
-    # cv2.imshow("Duplicate", transformed_image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()                         
+    print(filename)
